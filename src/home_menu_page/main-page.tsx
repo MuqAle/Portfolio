3ca239436a4ac6cbe58dc1fd45002d3c160026d7
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2024-05-20 13:20:32 -0400

    create time compoent & styling

--- src/home_menu_page/main-page.tsx
+++ src/home_menu_page/main-page.tsx
@@ -1,14 +1,37 @@
+import { useEffect } from "react"
+import { useImmer } from "use-immer"
+
 
 const MainHomePage = () => {
 
+    const [time, setTime] = useImmer(new Date().toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' }))
+    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date().toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' }))
+        },1000)
+
+        return () => {
+            clearInterval(interval)
+        }
+    },[])
+
+    const formatTime = time.split(' ')
+    
     return(
         <section id="main-home-container">
             <div className="top-part">
                 <div></div>
             </div>
-            <div className='bottom-part'></div>
+            <div className='bottom-part'>
+                <div className="time-container">
+                    <p className="time">{formatTime[0]}</p>
+                    <p className="time-period">{formatTime[1]}</p>
+                </div>
+            </div>
         </section>
     )
+
 }
 
 export default MainHomePage
