e40cfc631fc6ce882c97ff5e9b8a0fbd6f7b86c6
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2024-06-19 03:52:05 -0400

    create ecommerce project home animation

--- src/home_menu_page/ecommerce-project-wii-box.tsx
+++ src/home_menu_page/ecommerce-project-wii-box.tsx
@@ -1,23 +1,45 @@
+import { Updater, useImmer } from 'use-immer'
 import outline from '../images/home-menu/ecommerce-project-bg.svg'
 import logo from '../images/home-menu/ecommerce-project-logo.svg'
-import {motion} from "framer-motion"
+import {AnimatePresence, motion} from "framer-motion"
+import ChannelOverlay from '../project-carousel/overlay'
 
-const EcommerceProjectWiiBox = () => {
+const EcommerceProjectWiiBox = ({onClick,zoom,setZoom}: {
+  onClick:(event: any) => void,
+  setZoom:Updater<boolean>,
+  zoom:boolean}) => {
+    const [active, setActive] = useImmer(false)
     return(
-        <li className="wii-box">
+        <li className='wii-box-list'>
+          <AnimatePresence>
+          {
+            active && 
+            (
+              <ChannelOverlay setZoom={setZoom} onClick={() => setActive(false)}></ChannelOverlay>
+            )
+          }
+          </AnimatePresence>
+            <div className="wii-box" onClick={(event) => {
+              onClick(event)
+              setActive(true)
+              }}>
+         
             <img className='wii-outline' src={outline} alt="grey outline" />
             <motion.img className="project-logo" src={logo} alt="gem heart with muqqy qritten underneath" 
             animate={{
-                y:['0%','-15%','0%,15%']
+                y:zoom ? '0':['0%','-15%','0%,15%'] 
             }}
-            transition={{
+            transition={
+              zoom ? {}:{
                 duration: 2,
                 repeat: Infinity, 
                 repeatType: 'loop', 
                 ease: 'easeInOut', 
               }}
             />
+        </div>
         </li>
+      
     )
 }
 
